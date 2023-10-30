3e28cb8e86779aa228387b623caf00d77fe0c67f
Kokosha Danil <danilkokosha@gmail.com>
2023-10-30 14:30:34 +0300

    change main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
-#include <iostream>
-
-using namespace std;
-
 int main()
 {
     int A,B,C;
     
     
     cout << "Введите целое число A: ";
-    cin >> A; cout << endl;
+    numA(A); cout << endl;
     
     cout << "Введите целое число B: ";
-    cin >> B; cout << endl;
+    numB(B); cout << endl;
     
     cout << "Введите целое число C: ";
-    cin >> C; cout << endl;
+    numC(C); cout << endl;
     
-    int delenie1 = (A-B)%C;
-    int delenie2 = A / (B+C);
     
-    cout << "Остаток от деления разности чисел А и В на число С: " << delenie1 << endl;
-    cout << "Целая часть от деления числа А на сумму чисел В и С: " << delenie2 << endl;
+    cout << "Остаток от деления разности чисел А и В на число С: " << delenie1(A,B,C) << endl;
+    cout << "Целая часть от деления числа А на сумму чисел В и С: " << delenie2(A,B,C) << endl;
 }
