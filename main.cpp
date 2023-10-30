d1f366676f07c4136ec071dff14392da95ead732
Br0k0da <danilkokosha@gmail.com>
2023-10-30 17:25:27 +0300

    change numA

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-void numA(int &pA){
-q
+int numA(string pA, bool& T) {
     cin >> pA;
+    int pa = atoi(pA.c_str());
+    if (pa > 0) { return pa; }
+    else { cout << endl << "Необходимо ЧИСЛО больше ноля!!!" << endl; T = 0; }
 }
