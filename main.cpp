4a13a149c403e15335605a03cf4d9db337c97afb
Br0k0da <danilkokosha@gmail.com>
2023-10-30 17:26:09 +0300

    change numB

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
-void numB(int &pC){
-    cin >> pC;
+int numB(string pB, bool& T) {
+    cin >> pB;
+    int pb = atoi(pB.c_str());
+    if (pb > 0) { return pb; }
+    else { cout << endl << "Необходимо ЧИСЛО больше ноля!!!" << endl; T = 0; }
 }
