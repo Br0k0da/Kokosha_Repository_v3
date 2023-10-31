7027fc4f24c235368cf7abef2daaa74e66ef975f
Br0k0da <danilkokosha@gmail.com>
2023-10-31 15:41:30 +0300

    Change numB 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 int numB(string pB, bool& T) {
+    cout << "Введите целое число B (>0): ";
     cin >> pB;
     int pb = atoi(pB.c_str());
-    if (pb > 0) { return pb; }
-    else { cout << endl << "Необходимо ЧИСЛО больше ноля!!!" << endl; T = 0; }
+    if (pb > 0) {T=1; return pb; }
+    else { cout << endl << "!!!!!!!!Необходимо ЧИСЛО больше ноля!!!!!!!!" << endl;}
 }
