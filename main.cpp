62268507a95322b374fa122d65f4ebd5ef02201c
Br0k0da <91027272+Br0k0da@users.noreply.github.com>
2023-11-13 12:07:46 +0300

    change main

--- main.cpp
+++ main.cpp
@@ -40,28 +40,48 @@ int main()
 {
     setlocale(LC_ALL, "");
     string A, B, C;
-    int a, b, c;
+    int a = 0, b = 0, c = 0, X;
     bool T = 0;
 
-
-    while(T != 1) {
-        a = numA(A, T); cout << endl;
-    }
-    T = 0;
-
-    while(T != 1) {
-        b = numB(B, T); cout << endl;
-    }
-    T = 0;
+    cout << "1. Ввести A" << endl;
+    cout << "2. Ввести B" << endl;
+    cout << "3. Ввести C" << endl;
+    cout << "4. Остаток от деления разности чисел А и В на число С" << endl;
+    cout << "5. Целая часть от деления числа А на сумму чисел В и С" << endl;
     
-    while(T != 1) {
-        c = numC(C, T); cout << endl;
-    }
-
-
-    cout << "Остаток от деления разности чисел А и В на число С: " << delenie1(a, b, c) << endl;
-    cout << "Целая часть от деления числа А на сумму чисел В и С: " << delenie2(a, b, c) << endl;
+    do
+    {
+       cin >> X;
+       switch(X)
+       {
+            case 1:
+            while(T != 1) {
+                a = numA(A, T); cout << endl;
+            }
+            T = 0;
+            break;
+            
+            case 2:
+            while(T != 1) {
+                b = numB(B, T); cout << endl;
+            }
+            T = 0;
+            break;
+            
+            case 3:
+            while(T != 1) {
+                c = numC(C, T); cout << endl;
+            }
+            T = 0;
+            break;
+            
+            case 4:
+            cout << "Остаток от деления разности чисел А и В на число С: " << delenie1(a, b, c) << endl;
+            break;
+            
+            case 5:
+            cout << "Целая часть от деления числа А на сумму чисел В и С: " << delenie2(a, b, c) << endl;
+            break;
+       }
+    }while(true);
 }
-
-
-
