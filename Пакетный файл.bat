1fe58924d4650724ffb1921984904daf4de39db1
Br0k0da <danilkokosha@gmail.com>
2023-11-29 18:27:37 +0300

    change

---
+++ Пакетный файл.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" project_test.sln /build Debug
+"C:\Users\1\Desktop\doxygen" myConfig
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" build.iss
